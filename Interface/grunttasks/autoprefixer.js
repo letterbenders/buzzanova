bd517c2d76a6790436be97832d7aa5cb619c8c0f
soerensigfusson <soerensigfusson@me.com>
2015-05-09 16:14:08 +0200

    new build

--- Interface/grunttasks/autoprefixer.js
+++ Interface/grunttasks/autoprefixer.js
@@ -5,4 +5,8 @@ module.exports = {
     prototype: {
 		src: '<%= package.prototyperesources %>/css/styles.css' // globbing is also possible here
     },
+    production: {
+        src: '<%= package.buildresources %>/css/styles.css' // globbing is also possible here
+    }
+
   }
