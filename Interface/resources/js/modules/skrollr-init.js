14a344fd147b0ef2fb5f30306ff747da55488eb0
soerensigfusson <soerensigfusson@me.com>
2015-06-04 21:48:06 +0200

    fix header on ipad og centered sections

--- Interface/resources/js/modules/skrollr-init.js
+++ Interface/resources/js/modules/skrollr-init.js
@@ -9,6 +9,20 @@ define(['jquery', 'skrollr'], function($, skrollr) {
                 $(element).trigger('skrollr', {name:name, direction:direction});
             }
         });
+    } else {
+        var isCompact = false
+        var $pageheader = $('.page-header');
+
+        $(window).on('scroll', function(){
+            var st = $(document).scrollTop();
+            if (st==0) {
+                $pageheader.removeClass('compact');
+                isCompact = false;
+            } else if (!isCompact) {
+                $pageheader.addClass('compact');
+                isCompact = true;
+            }
+        });
     }
 
 });
